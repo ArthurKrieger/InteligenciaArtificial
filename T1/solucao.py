e52b0603e9e8c9defc5c4a12debef09d2f321615
ArthurKrieger <arthurlkrieger@gmail.com>
2022-12-08 00:34:29 -0300

    basic setup

---
+++ T1/solucao.py
@@ -0,0 +1,108 @@
+class Nodo:
+    """
+    Implemente a classe Nodo com os atributos descritos na funcao init
+    """
+    def __init__(self, estado, pai, acao, custo):
+        """
+        Inicializa o nodo com os atributos recebidos
+        :param estado:str, representacao do estado do 8-puzzle
+        :param pai:Nodo, referencia ao nodo pai, (None no caso do nó raiz)
+        :param acao:str, acao a partir do pai que leva a este nodo (None no caso do nó raiz)
+        :param custo:int, custo do caminho da raiz até este nó
+        """
+        # substitua a linha abaixo pelo seu codigo
+        raise NotImplementedError
+
+
+def sucessor(estado):
+    """
+    Recebe um estado (string) e retorna uma lista de tuplas (ação,estado atingido)
+    para cada ação possível no estado recebido.
+    Tanto a ação quanto o estado atingido são strings também.
+    :param estado:
+    :return:
+    """
+    # substituir a linha abaixo pelo seu codigo
+    lista = []
+    pos = estado.find("_")
+    if(pos != 0 != 3 !=6):
+        list.append("esquerda",changePos(estado,pos,pos-1))
+    if(pos !=2 !=4 !=8):
+         list.append("direita",changePos(estado,pos,pos+1))
+    if(pos !=1 !=2 !=3):
+         list.append("abaixo",changePos(estado,pos,pos-3))
+    if(pos !=6 !=7 !=8):
+         list.append("acima",changePos(estado,pos,pos+3))
+    return lista       
+      
+
+
+
+def expande(nodo):
+    """
+    Recebe um nodo (objeto da classe Nodo) e retorna um iterable de nodos.
+    Cada nodo do iterable é contém um estado sucessor do nó recebido.
+    :param nodo: objeto da classe Nodo
+    :return:
+    """
+    # substituir a linha abaixo pelo seu codigo
+    raise NotImplementedError
+
+
+def bfs(estado):
+    """
+    Recebe um estado (string), executa a busca em LARGURA e
+    retorna uma lista de ações que leva do
+    estado recebido até o objetivo ("12345678_").
+    Caso não haja solução a partir do estado recebido, retorna None
+    :param estado: str
+    :return:
+    """
+    # substituir a linha abaixo pelo seu codigo
+    raise NotImplementedError
+
+
+def dfs(estado):
+    """
+    Recebe um estado (string), executa a busca em PROFUNDIDADE e
+    retorna uma lista de ações que leva do
+    estado recebido até o objetivo ("12345678_").
+    Caso não haja solução a partir do estado recebido, retorna None
+    :param estado: str
+    :return:
+    """
+    # substituir a linha abaixo pelo seu codigo
+    raise NotImplementedError
+
+
+def astar_hamming(estado):
+    """
+    Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Hamming e
+    retorna uma lista de ações que leva do
+    estado recebido até o objetivo ("12345678_").
+    Caso não haja solução a partir do estado recebido, retorna None
+    :param estado: str
+    :return:
+    """
+    # substituir a linha abaixo pelo seu codigo
+    raise NotImplementedError
+
+
+def astar_manhattan(estado):
+    """
+    Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Manhattan e
+    retorna uma lista de ações que leva do
+    estado recebido até o objetivo ("12345678_").
+    Caso não haja solução a partir do estado recebido, retorna None
+    :param estado: str
+    :return:
+    """
+    # substituir a linha abaixo pelo seu codigo
+    raise NotImplementedError
+
+def changePos(estado,x,y):
+    lista = list(estado)
+    x = lista[x]
+    lista[x]=lista[y]
+    lista[y]=x
+    return "".join(lista)
